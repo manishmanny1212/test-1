94ae6dc3316e970982c3cef05a451692dd3c8903
Manish Kumar <manish.kumar@moonraft.com>
2022-10-02 02:24:19 +0530

    day1 commit :basic card and delete button setup

--- to-do/src/App.js
+++ to-do/src/App.js
@@ -1,9 +1,15 @@
-
-
-
+import "./app.css";
 function App() {
   return (
-    <div>hello</div>
+    <div>
+      <h1>Todo APP</h1>
+      <div className="card">
+        <h2>title</h2>
+        <div className="actions">
+          <button className="btn">delete</button>
+        </div>
+      </div>
+    </div>
   );
 }
 
